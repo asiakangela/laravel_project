bfffd7042339c57078e596ab9771b902fe8afbdd
asiakN <nathaniel.asiak@acity.edu.gh>
2024-11-20 00:58:07 +0000

    First commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel = "stylesheet" href = "/app.css">
+
+
+
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</h1>
+        <p>
+        Open the command line.
+    Set your username: git config --global user.name "FIRST_NAME LAST_NAME"
+    Set your email address: git config --global user.email "MY_NAME@example.com"
+
+
+
+
+        </p>
+    </article>
+
+
+
+
+
+
+<a href="/">Go Back</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,72 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel = "stylesheet" href = "/app.css">
+
+
+
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</h1>
+        <p>
+        Open the command line.
+    Set your username: git config --global user.name "FIRST_NAME LAST_NAME"
+    Set your email address: git config --global user.email "MY_NAME@example.com"
+
+
+
+
+        </p>
+    </article>
+
+
+
+
+
+
+    
+    <article>
+        <h1><a href="/post">My Second Post</h1>
+        <p>
+        Open the command line.
+    Set your username: git config --global user.name "FIRST_NAME LAST_NAME"
+    Set your email address: git config --global user.email "MY_NAME@example.com"
+
+
+
+
+        </p>
+    </article>
+
+
+
+
+
+
+
+
+
+
+    
+    <article>
+        <h1><a href="/post">My Third Post</h1>
+        <p>
+        Open the command line.
+    Set your username: git config --global user.name "FIRST_NAME LAST_NAME"
+    Set your email address: git config --global user.email "MY_NAME@example.com"
+
+
+
+
+        </p>
+    </article>
+
+
+
+
+
+
+
+
+</body>
