160c1e98f67ddab46dd6513af02ebd6e2027de98
asiakN <nathaniel.asiak@acity.edu.gh>
2024-11-21 23:51:40 +0000

    changes

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -12,7 +12,7 @@
         <p>
         Open the command line.
     Set your username: git config --global user.name "FIRST_NAME LAST_NAME"
-    Set your email address: git config --global user.email "MY_NAME@example.com"
+    Set your email address: git config -- global user.email "MY_NAME@example.com"
 
 
 
